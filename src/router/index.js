f14ad544018fdf7e15363b7512ad8715e0020543
louis-25 <20151577@vision.hoseo.edu>
2020-09-15 13:38:07 +0900

    Session_test

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// const cors = require('cors')
 
 Vue.use(VueRouter)
+// Vue.use(cors())
 
 const routes = [
   {
@@ -17,6 +19,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/session_test',
+    name: 'session',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Session_test.vue')
   }
 ]
 
